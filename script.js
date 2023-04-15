9acbe87911d1035aeaa1b5a8d01692ef9f4b89c5
Beck <ulugbek_5755@mail.ru>
2023-04-15 16:38:56 +0200

    function answer added

--- script.js
+++ script.js
@@ -54,5 +54,57 @@ let questions = [
         "answer_3": "Tim Berner-Lee",
         "answer_4": "Justin Bieber",
         "right_answer": 3
+    },
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berner-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
     }
 ];
+
+
+let currentQuestion = 0;
+
+
+function init(){
+    document.getElementById('all_questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
+
+
+function answer(selection){
+    let question = questions[currentQuestion];
+
+    console.log('Selected answer is ', selection);
+    let selectionQuestionNumber = selection.slice(-1);
+    console.log('selectionQuestionNumber is', selectionQuestionNumber);
+    console.log('Current question ist ', question['right_answer']);
+
+    let idRightAnswer = `answer_${question['right_answer']}`;
+ 
+    if (selectionQuestionNumber == question['right_answer']){
+        console.log('Richtige Antwort !');
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        console.log ('Falsche Antwort !');
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idRightAnswer).parentNode.classList.add('bg-success');
+    }
+
+    document.getElementById('next-button').disabled = false;
+}
